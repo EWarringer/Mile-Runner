58b127f98b7ab411f382f7c1d257f5d11bc2bc22
EWarringer <erikwarringer@gmail.com>
2015-11-10 17:37:47 -0500

    migrated tracks table

--- db/migrate/20151106003349_tracks.rb
+++ db/migrate/20151106003349_tracks.rb
@@ -1,4 +1,9 @@
 class Tracks < ActiveRecord::Migration
   def change
+    create_table :tracks do |table|
+      table.string :name
+      table.string :length
+      table.string :city
+    end
   end
 end
